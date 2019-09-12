977ca16cfd27e4e5449ad3192cd62f3621bb8254
Skyler Pryor <Skylerhovey24@icloud.com>
2019-09-12 10:33:37 -0400

    calculates correct days between b day and now

--- birthday.py
+++ birthday.py
@@ -1,3 +1,40 @@
+import datetime
 from banner import banner
 
-banner("  Birthday", "Skyler")
+banner("BIRTHDAY", "Skyler")
+
+
+
+
+
+
+
+def main ():
+    birthday = get_birthday_info()
+    today = datetime.date.today()
+    num_days = compute_days_between_dates(birthday, today)
+    print_birthday_info(num_days)
+
+
+
+def get_birthday_info():
+    print("When were you born?")
+    year = int(input("Year [YYYY]: "))
+    month = int(input("Month [MM]: "))
+    day = int(input("Day [DD]: "))
+
+    birthday = datetime.date(year, month, day)
+    return birthday
+
+
+def compute_days_between_dates(original_date, target_date):
+    current_year = datetime.date(target_date.year, original_date.month,original_date.day)
+    dt = current_year - target_date
+
+    return dt.days
+
+def print_birthday_info(number_of_days):
+    print(number_of_days)
+
+
+main()
