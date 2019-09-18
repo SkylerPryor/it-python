ce9971c055358d942416c4cc98e7a89c5452240a
Skyler Pryor <Skylerhovey24@icloud.com>
2019-09-18 09:56:45 -0400

    Zip code 1 hr

---
+++ zipcode.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+from banner import banner
+banner("ZIP CODE SORTER","Skyler")
+
+
+print("")
+print("Welcome To The Computer's Zip Code Sorter For Newaygo County.")
+print("")
+name = input("Hello What is your name?")
+print("")
+print(f"Hello {name} Welcome To Zip Code Sorter please enter a 5 digit zipcode below.")
+print("")
+choice = int(input("Enter Zip Code Here:"))
+
+if choice == 49412 or 49413:
+    print("This Is Fremont's Zip Code.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
